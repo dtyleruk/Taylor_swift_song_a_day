2717a7557a179e543807474847f0763595a5de48
dtyleruk <32400245+dtyleruk@users.noreply.github.com>
2024-10-20 08:29:38 +0100

    Move song urls
    Attempt to embed song with url sitting in script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+// Array of Taylor Swift songs and their corresponding Spotify embed URLs
 const songs = [
-    "Love Story",
-    "Blank Space",
-    "Shake It Off",
-    "All Too Well",
-    "Cardigan",
-    // Add more songs...
+    { title: "Love Story", url: "https://open.spotify.com/track/1D4PL9B8gOg78jiHg3FvBb" },
+    { title: "Blank Space", url: "https://open.spotify.com/track/1u8c2t2Cy7UBoG4ArRcF5g" },
+    { title: "Shake It Off", url: "https://open.spotify.com/track/0V3wPSX9ygBnCm8psDIegu" }
 ];
 
-const today = new Date();
-const dayOfYear = Math.floor((today - new Date(today.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
-const songOfTheDay = songs[dayOfYear % songs.length];
+// Function to get a random song
+function getRandomSong() {
+    const randomIndex = Math.floor(Math.random() * songs.length);
+    return songs[randomIndex];
+}
 
-document.getElementById("song").innerText = songOfTheDay;
+// Get a random song
+const song = getRandomSong();
+
+// Set the song title and iframe src
+document.getElementById('song').innerText = song.title;
+document.getElementById('songIframe').src = song.url;
